4a05ebd5c7b6b2782a7fc326b31dabdaf6782f9e
MashB <mahesh_badmanji@yahoo.com>
2023-06-07 12:26:13 +0530

    Renamed test folder and added enumeration

--- jest.config.ts
+++ jest.config.ts
@@ -7,7 +7,7 @@ module.exports = {
   coverageProvider: "v8",
   moduleFileExtensions: ["js", "jsx", "ts", "tsx", "json", "node"],
   // roots: ["<rootDir>"],
-  testMatch: ["**/__test__/*.ts"],
+  testMatch: ["**/test/*.ts", "**/?(*.)+(spec|test).[tj]s?(x)"],
   transform: {
     "^.+\\.(ts|tsx|js)$": "ts-jest",
   },
